25a9f05fd5fa53036432e96ced5d0418879ef1a9
Alex Greif <alex.greif@greifdesign.net>
2016-05-19 11:55:33 +0200

    bootstrap template

--- parent-child/Foundation.hs
+++ parent-child/Foundation.hs
@@ -80,14 +80,14 @@ instance Yesod App where
         withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 
     -- The page to be redirected to when authentication is required.
-    authRoute _ = Just $ AuthR LoginR
+    -- authRoute _ = Just $ AuthR LoginR
 
-    -- Routes not requiring authentication.
-    isAuthorized (AuthR _) _ = return Authorized
-    isAuthorized FaviconR _ = return Authorized
-    isAuthorized RobotsR _ = return Authorized
-    -- Default to Authorized for now.
-    isAuthorized _ _ = return Authorized
+    -- -- Routes not requiring authentication.
+    -- isAuthorized (AuthR _) _ = return Authorized
+    -- isAuthorized FaviconR _ = return Authorized
+    -- isAuthorized RobotsR _ = return Authorized
+    -- -- Default to Authorized for now.
+    -- isAuthorized _ _ = return Authorized
 
     -- This function creates static content files in the static folder
     -- and names them based on a hash of their content. This allows
@@ -126,31 +126,31 @@ instance YesodPersist App where
 instance YesodPersistRunner App where
     getDBRunner = defaultGetDBRunner appConnPool
 
-instance YesodAuth App where
-    type AuthId App = UserId
+-- instance YesodAuth App where
+--     type AuthId App = UserId
 
-    -- Where to send a user after successful login
-    loginDest _ = HomeR
-    -- Where to send a user after logout
-    logoutDest _ = HomeR
-    -- Override the above two destinations when a Referer: header is present
-    redirectToReferer _ = True
+--     -- Where to send a user after successful login
+--     loginDest _ = HomeR
+--     -- Where to send a user after logout
+--     logoutDest _ = HomeR
+--     -- Override the above two destinations when a Referer: header is present
+--     redirectToReferer _ = True
 
-    authenticate creds = runDB $ do
-        x <- getBy $ UniqueUser $ credsIdent creds
-        case x of
-            Just (Entity uid _) -> return $ Authenticated uid
-            Nothing -> Authenticated <$> insert User
-                { userIdent = credsIdent creds
-                , userPassword = Nothing
-                }
+--     authenticate creds = runDB $ do
+--         x <- getBy $ UniqueUser $ credsIdent creds
+--         case x of
+--             Just (Entity uid _) -> return $ Authenticated uid
+--             Nothing -> Authenticated <$> insert User
+--                 { userIdent = credsIdent creds
+--                 , userPassword = Nothing
+--                 }
 
-    -- You can add other plugins like Google Email, email or OAuth here
-    authPlugins _ = [authOpenId Claimed []]
+--     -- You can add other plugins like Google Email, email or OAuth here
+--     authPlugins _ = [authOpenId Claimed []]
 
-    authHttpManager = getHttpManager
+--     authHttpManager = getHttpManager
 
-instance YesodAuthPersist App
+-- instance YesodAuthPersist App
 
 -- This instance is required to use forms. You can modify renderMessage to
 -- achieve customized and internationalized form validation messages.
