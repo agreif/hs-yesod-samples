eb3770fd185d71042711978f5c7d010e97bf3577
Alex Greif <alex.greif@greifdesign.net>
2016-07-07 17:18:54 +0200

    added hashdb-auth sample

---
+++ hashdb-auth/README.md
@@ -0,0 +1,6 @@
+# HashSB-Auth sample
+
+features:
+- stores userId with hashed password in DB
+
+![pic1](pics/pic1.png)
---
+++ hashdb-auth/src/app/devel.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+import "hashdb-auth" Application (develMain)
+import Prelude (IO)
+
+main :: IO ()
+main = develMain
